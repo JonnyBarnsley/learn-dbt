c26320e5c315fdb29bb0c03b10a0937393a69fc2
jonny.barnsley@payroc.com <jonny.barnsley@payroc.com>
2021-10-26 10:49:56 +0000

    added payments and orders facts

---
+++ models/marts/core/FCT_ORDERS.sql
@@ -0,0 +1,27 @@
+with payments as(
+    select * from {{ref('STG_PAYMENTS')}}
+),
+
+orders as (
+    select * from {{ref('STG_ORDERS')}}
+),
+
+order_payments as(
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by order_id
+),
+
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+    from orders
+    left join order_payments using (order_id)
+)
+
+SELECT * FROM final
